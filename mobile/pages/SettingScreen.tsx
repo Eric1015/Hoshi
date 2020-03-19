28dec08aa0f943563a2c1a87a031d1e06325c607
Eric1015 <love1dbsb77@hotmail.com>
2020-03-18 17:19:20 -0700

    Implemented getting the geolocation from the user

--- mobile/pages/SettingScreen.tsx
+++ mobile/pages/SettingScreen.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
+
 import i18n from '../localization/i18n';
+import { sendGeolocation } from '../api';
 
 export default function SettingScreen(props) {
+    const handleSendGeolocation = async () => {
+        const { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status === 'granted') {
+            const location = await Location.getCurrentPositionAsync({});
+            sendGeolocation({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+            })
+        }
+    }
+
+    useEffect(() => {
+        handleSendGeolocation();
+    }, []);
+
     return (
         <View style={styles.container}>
             <Text style={styles.center_text}>{i18n.t('welcome_to_hoshi')}</Text>
